aee81f10f66e6955ebcca0cde2ee9e47eb83bcd8
Adam Starbuck <acs11e@acu.edu>
2015-02-02 16:15:03 -0600

    Changed index.html to index.php for user management and also because PHP rocks. If you set up your XAMPP stuff correctly, you should be able to 'Sign in' but not really because there is no authentication right now, just a query that goes through and says 'yes, email=x and pass=y' So we need to get some kind of authentication in there.

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+
+$link = new mysqli("localhost","root","","project1"); /*for local testing only*/
+
+
+//link database
+if ($link->connect_errno) {
+    printf("Connect failed: %s\n", $link->connect_error);
+    exit();
+}
+//do stuff
+if(isset($_REQUEST["action"]))
+	$action = $_REQUEST["action"];
+else
+	$action = "none";
+
+$message = "";
+
+if($action == "login_user")
+{
+	$email = $_POST["email"];
+    $passwrd= $_POST["password"];
+	$email = htmlentities($link->real_escape_string($email));
+	$result = $link->query("SELECT * from users where email= '$email' AND pass= '$passwd'");
+	if(!$result)
+		die ('Can\'t query users because: ' . $link->error);
+	else
+		header("Location: http://localhost/WD2-project1/portal");
+        die();
+
+}
+
+// elseif ($action == "delete_user") {
+// 	$id = $_POST["id"];
+// 	$id = htmlentities($link->real_escape_string($id));
+// 	$result = $link->query("delete from users where id='" . $id . "'");
+// 	if(!$result)
+// 		die ('Can\'t query users because: ' . $link->error);
+// 	else
+// 		$message = "User Deleted";
+// }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -7,9 +52,6 @@
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<!--[if lt IE 9]>
-			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
 		<link href="css/styles.css" rel="stylesheet">
 	</head>
 	<body style="margin-top:50px">
@@ -22,7 +64,7 @@
                         <h3 class="panel-title"><strong>Generic Company</strong></h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form">
+                        <form role="form" method="post" action="index.php">
                             <fieldset>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
@@ -36,7 +78,8 @@
                                     </label>
                                 </div>
                                 <!-- Change this to a button or input when using this as a form -->
-                                <a href="portal/index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                                <input type="hidden" name="action" value="login_user" />
+                                <button class="btn btn-lg btn-success btn-block">Login</button>
                             </fieldset>
                         </form>
                     </div>
@@ -45,7 +88,7 @@
         </div>
     </div>
 	<!-- script references -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
